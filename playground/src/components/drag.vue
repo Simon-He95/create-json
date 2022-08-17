d03ac06916485a69c95468a15a7d8b0cf8aa46d5
Simon-He95 <674949287@qq.com>
2022-08-17 08:33:13 +0800

    chore: update

---
+++ playground/src/components/drag.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { MenuItem, SimpleMenu } from '@strapi/design-system/SimpleMenu'
+import { Button } from '@strapi/design-system/Button'
+const editLayoutRemainingFields = []
+</script>
+
+<template>
+  <div>
+    <SimpleMenu id="label" :as="Button" data-testid="add-field" full-width variant="secondary">
+      <template v-for="field in editLayoutRemainingFields" :key="field">
+        <MenuItem on-click="() => onAddField(field)">
+          {{ field }}
+        </MenuItem>
+      </template>
+    </SimpleMenu>
+  </div>
+</template>
